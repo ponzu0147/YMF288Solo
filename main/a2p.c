262a887bd25a805831d43741a6d0209100c26d7f
ponzu0147 <firecracker.rebirth@gmail.com>
2022-01-11 10:34:48 +0900

    a2p.c changed

--- main/a2p.c
+++ main/a2p.c
@@ -14,27 +14,24 @@ const int mode_f[] = {
 // BigEndian to LitteleEndian
 int xcg_int(int _a){
     int a = _a;
-    int ret=0;
-    ret = a>>8;
-    int ret2=0;
+    int ret = 0;
+    ret = a >> 8;
+    int ret2 = 0 ;
     ret2 = (a & 0xff); 
-    a = ret + (ret2<<8);
-
+    a = ret + (ret2 << 8);
     return a;
 }
 
 int get_deltan(int cc, int ddelta_n){
     int nplus;
-    //printf(" data=%d ",cc);
     int f = mode_f[cc] ;
-    printf(" f = %03d", f);
-    nplus = (f * ddelta_n) >> 6; // 64で割る
-    
-    printf(" n+ = %03d ",nplus);
+    //printf(" f = %03d", f);
+    nplus = (f * ddelta_n) >> 6; // Divide by 64.
+    //printf(" n+ = %03d ",nplus);
 
-    if (nplus < 128){
+    if (nplus < 128) {
         nplus = 127;
-    } else if ( nplus > 24576){
+    } else if ( nplus > 24576 ) {
         nplus = 24576;
     }
     delta_n = nplus;
@@ -44,13 +41,12 @@ int get_deltan(int cc, int ddelta_n){
 int get_x(int _data, int _delta_x, int _delta_n){
     u_int8_t c = _data;
     int d;
-    //int x;
     int n;
     int16_t x;
 
-    for ( int i=3; i >= 0; i-- ){
-        bit[i] = (c >> i ) & 1;
-        printf("%x",bit[i]);
+    for (int i = 3; i >= 0; i--) {
+        bit[i] = (c >> i) & 1;
+        //printf("%x",bit[i]);
     }
 
     int delta_n = get_deltan(c,_delta_n);
@@ -58,9 +54,9 @@ int get_x(int _data, int _delta_x, int _delta_n){
     c = c & 7;
     c = c << 1 + 1;
 
-    printf("(delta_n: %05d) ",delta_n);
-    parts_x= (1 - 2 * bit[3]) * (8*bit[2] + 4*(bit[1]) + 2*(bit[0]) + 1) * delta_n;
-    if( bit[3] == 1 ){
+    //printf("(delta_n: %05d) ",delta_n);
+    parts_x= (1 - 2 * bit[3]) * (8 * bit[2] + 4 * bit[1] + 2 * bit[0] + 1) * delta_n;
+    if(bit[3] == 1){
         parts_x = (parts_x >> 3) +1;
     } else {
         parts_x = parts_x >> 3;
@@ -73,7 +69,7 @@ int get_x(int _data, int _delta_x, int _delta_n){
         x = x + 1;
         }
 
-    printf("x= %03d ", x);
+    //printf("x= %03d ", x);
     return x; // Xn+1
 }
 
@@ -89,7 +85,7 @@ void main(void){
         c = fgetc(fp);
         if(feof(fp)) break;
 
-        printf("%05d ",num);
+        //printf("%05d ",num);
 
         ch = c >> 4;
         cl = c & 15;
@@ -99,9 +95,9 @@ void main(void){
         delta_x2 = get_x(cl,delta_x,delta_n);
         int x2_xcg = xcg_int(delta_x2);
 
-        printf(" %04x %04x\n",x_xcg,x2_xcg);
-        fwrite(&x_xcg,2,1,pp);
-        fwrite(&x2_xcg,2,1,pp);
+        //printf(" %04x %04x\n",x_xcg,x2_xcg);
+        fwrite(&x_xcg, 2, 1, pp);
+        fwrite(&x2_xcg, 2, 1, pp);
         
         delta_x = delta_x2;
         num++;
